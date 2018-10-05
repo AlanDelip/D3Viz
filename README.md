bf8a123e109ad309b564a98760cdf991f81e2937
AlanDelip <zx2245@columbia.edu>
2018-10-04 21:32:04 -0400

    comments for simple bar chart add README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Visualization works with D3JS
+> For display, interaction, and analysis of the projects, please visit [Alan's Site](https://alan-zhufengxu.com "Alan's Site") or my [Observable homepage](https://beta.observablehq.com/@alandelip "Observable Notebook for d3").
+
+### Bar Chart
+- simple bar chart
+- horizontal bar chart (TODO)
+- stacked bar chart (TODO)
+- bar chart with label and interaction (TODO)
+- bar chart with average line (TODO)
+- bar chart with customized reference line (TODO)
+- bar chart with timeline (TODO)
+
+### Line Chart
+
+### Pie Chart
+
+### Scatter Plot
+
+### Map
--- barcharts/simple-barchart.js
+++ barcharts/simple-barchart.js
@@ -1,43 +1,22 @@
 let d3 = require('d3');
-let rawData = [
-	{letter: "A", frequency: .08167},
-	{letter: "B", frequency: .01492},
-	{letter: "C", frequency: .02782},
-	{letter: "D", frequency: .04253},
-	{letter: "E", frequency: .12702},
-	{letter: "F", frequency: .02288},
-	{letter: "G", frequency: .02015},
-	{letter: "H", frequency: .06094},
-	{letter: "I", frequency: .06966},
-	{letter: "J", frequency: .00153},
-	{letter: "K", frequency: .00772},
-	{letter: "L", frequency: .04025},
-	{letter: "M", frequency: .02406},
-	{letter: "N", frequency: .06749},
-	{letter: "O", frequency: .07507},
-	{letter: "P", frequency: .01929},
-	{letter: "Q", frequency: .00095},
-	{letter: "R", frequency: .05987},
-	{letter: "S", frequency: .06327},
-	{letter: "T", frequency: .09056},
-	{letter: "U", frequency: .02758},
-	{letter: "V", frequency: .00978},
-	{letter: "W", frequency: .02360},
-	{letter: "X", frequency: .00150},
-	{letter: "Y", frequency: .01974},
-	{letter: "Z", frequency: .00074}
-];
+import {cate_cont} from '../data/xy-data'
 
-data = rawData
+// clean/analyze/convert the data
+let data = cate_cont
 	.slice()
 	.sort((a, b) => b.frequency - a.frequency)
 	.map(({letter, frequency}) => ({name: letter, value: frequency}));
 
-let svg = d3.select("svg");
+// define svg work zone and its appearance (width, height, margin, padding)
+let svg = d3.select("#simple-barchart");
 let margin = {top: 20, right: 0, bottom: 30, left: 40};
-let height = 500, width = 900;
+let height = svg.attr("height"), width = svg.attr("width");
 
+// mapping function
+// for a xy-coordinate, we should have x() and y() mapping functions. Each contains a domain and a mapping range.
 let x = d3.scaleBand()
+	// scaleBand() used for building up bands with averaged width and padding
+	// in this case, we are mapping ordinal domains to a continuous range
 	.domain(data.map(d => d.name))
 	.range([margin.left, width - margin.right])
 	.padding(0.1);
@@ -46,26 +25,34 @@ let y = d3.scaleLinear()
 	.domain([0, d3.max(data, d => d.value)]).nice()
 	.range([height - margin.bottom, margin.top]);
 
+// configurations of axis
 let xAxis = g => g
+	// small tweaks because of margin
 	.attr("transform", `translate(0,${height - margin.bottom})`)
-	.call(d3.axisBottom(x)
-		.tickSizeOuter(0));
+	// Invokes the specified function exactly once, passing in this selection along with any optional arguments.
+	// Returns this selection. This is equivalent to invoking the function by hand but facilitates method chaining.
+	.call(d3.axisBottom(x).tickSizeOuter(0));
 
 let yAxis = g => g
 	.attr("transform", `translate(${margin.left},0)`)
-	.call(d3.axisLeft(y))
+	.call(d3.axisLeft(y).tickSize(10).tickSizeOuter(0))
+	// .domain means axis here, not including ticks
 	.call(g => g.select(".domain").remove());
 
-svg.append("g")
-	.attr("fill", "steelblue")
-	.selectAll("rect").data(data).enter().append("rect")
-	.attr("x", d => x(d.name))
-	.attr("y", d => y(d.value))
-	.attr("height", d => y(0) - y(d.value))
-	.attr("width", x.bandwidth());
-
+// add axis
 svg.append("g")
 	.call(xAxis);
-
 svg.append("g")
 	.call(yAxis);
+
+// map real data to rect
+svg.append("g")
+	.attr("fill", "#4285f4")
+	.selectAll("rect").data(data).enter().append("rect")
+	// map x using x() from category/value to coordinates
+	.attr("x", d => x(d.name))
+	.attr("y", d => y(d.value))
+	// mapping of y: the top of the rect is the start of y.
+	.attr("height", d => y(0) - y(d.value))
+	// calculated bandwidth
+	.attr("width", x.bandwidth());
